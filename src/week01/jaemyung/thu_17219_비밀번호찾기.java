f5257c702ccdbded4b0b8638dcc5a989b6140386
jaemyungchoi95 <woaud130@gmail.com>
2025-02-03 00:01:40 +0900

    불필요파일 복원...

---
+++ src/week01/jaemyung/thu_17219_비밀번호찾기.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class thu_17219_비밀번호찾기 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+
+
+
+    }
+}
