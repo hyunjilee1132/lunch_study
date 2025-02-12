b9dbfe8510bfcc0ae3b529796513cd51c3479308
leeozzi <lstory1997l@gmail.com>
2025-02-13 04:54:11 +0900

    Revert "파일명 수정 & 패션왕신해빈 풀이"
    This reverts commit 0c4fd9361c514e5b124560afcf48e2e67de28c72.

--- src/week02/eonji/BOJ1463.java
+++ src/week02/eonji/BOJ1463.java
@@ -2,7 +2,7 @@ package Study.algorithm_lunchStudy;
 
 import java.util.*;
 
-public class BOJ1463_1로만들기 {  // 내가 푼 거 아니고 다른 사람 풀이 보고 이해함
+public class BOJ1463 {  // 내가 푼 거 아니고 다른 사람 풀이 보고 이해함
     /**
      * 1로 만들기
      */
@@ -11,8 +11,6 @@ public class BOJ1463_1로만들기 {  // 내가 푼 거 아니고 다른 사람 
         int N = sc.nextInt();
         int[] arr = new int[1000001];
 
-
-        // N의 경우의 수를 인덱스로 구분해서 다 담아줌
         arr[1] = 0;
         arr[2] = 1;
         arr[3] = 1;
@@ -24,7 +22,7 @@ public class BOJ1463_1로만들기 {  // 내가 푼 거 아니고 다른 사람 
             int temp1 = 1000001;
             int temp2 = 1000001;
             int temp3 = 1000001;
-            int min = 1000001;
+            int min = 100001;
 
             temp1 = arr[i-1] + 1;
 
