bcd159f08260241494d59aad26dc2d03ef019a63
leeozzi <lstory1997l@gmail.com>
2025-02-16 11:04:18 +0900

    구간합구하기, 2xN타일링

---
+++ src/week03/eonji/BOJ11726_2xN타일링.java
@@ -0,0 +1,20 @@
+package workingon;
+
+import java.util.Scanner;
+
+public class BOJ11726_2xN타일링 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] dp = new int[1001];
+        // dp의 인덱스는 전체 길이 n
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3 ; i < dp.length ; i++) {
+            dp[i] = (dp[i-2] + dp[i-1]) % 10007;
+        }
+
+        System.out.println(dp[n]);
+
+    }
+}
