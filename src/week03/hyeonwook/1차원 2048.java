73b4026b2b67aae8322ec58963edee9b4923161d
Namhyeonug <namhyeonug203@gmail.com>
2025-02-20 23:07:07 +0900

    Update 1차원 2048.java

--- src/week03/hyeonwook/1차원 2048.java
+++ src/week03/hyeonwook/1차원 2048.java
@@ -1,1 +1,34 @@
-
+import java.util.*;
+import java.io.*;
+public class boj27514_2048 {
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] list = new int[63]; // 2의 0승 부터 2의 62승까지 지수 저장
+		while(st.hasMoreTokens()) {
+			long number = Long.parseLong(st.nextToken());
+			if (number != 0) {
+				list[(int)(Math.log(number)/Math.log(2))] += 1;
+			}
+		}
+		for (int i = 0; i < 62; i++) {
+			int x = list[i];
+			list[i+1] += x/2;
+		}
+		boolean bigger_than_zero = false;
+		int pow2 = 0;
+		for (int x = 62; x >=0; x--) {
+			if (list[x] > 0) {
+				bigger_than_zero = true;
+				pow2 = x;
+				break;
+			}
+		}
+		long answer = 0;
+		if (bigger_than_zero) {
+			answer = (long)Math.pow(2, pow2);
+		}
+		System.out.println(answer);
+	}
+}
