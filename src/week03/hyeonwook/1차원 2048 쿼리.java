745372d72ff536dc4b90b6f77a5f6aa49386eb03
Namhyeonug <namhyeonug203@gmail.com>
2025-02-21 00:41:38 +0900

    Update 1차원 2048 쿼리.java

--- src/week03/hyeonwook/1차원 2048 쿼리.java
+++ src/week03/hyeonwook/1차원 2048 쿼리.java
@@ -1,1 +1,32 @@
-
+import java.io.*;
+public class Main {
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		long sum = 0;
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		for (int x = 0; x < n; x++) {
+			String s = br.readLine();
+			long l = Long.parseLong(s.substring(1));
+			if (l != 0) {
+				if (s.charAt(0) == '+') {
+					sum += l;
+				} else {
+					sum -= l;
+				}
+			}
+			long answer = 0;
+			if (sum != 0) {
+				int k = (int) (Math.log(sum)/Math.log(2));
+				if (k > 62) {
+					answer = (long) (Math.pow(2, 62));
+				} else {
+					answer = (long) (Math.pow(2, k));
+				}
+			}
+			bw.write(answer+"\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
